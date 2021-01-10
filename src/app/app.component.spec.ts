2835a853861c3d4657ecd38a8b8d027dabe597af
GeoAstronaute <jeanmarc.royy@gmail.com>
2021-01-10 18:41:42 +0100

    add simple component that uses ngx-monaco-editor + unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { filter, take } from 'rxjs/operators';
+import { MonacoEditorLoaderService, MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 import { AppComponent } from './app.component';
 
+function asyncTimeout(time) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(true), time);
+  });
+}
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let expectedCode: any = `{
+    "test": 1,
+    "scopes": { "test": 2 }
+}`;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
+        MonacoEditorModule
       ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
+
+    const monacoService = TestBed.inject(MonacoEditorLoaderService);
+    await monacoService.isMonacoLoaded$.pipe(
+      filter(loaded => !!loaded), take(1)
+    ).toPromise();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'monaco-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('monaco-testing');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('Check if code in editor is populated', async () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('monaco-testing app is running!');
+    await asyncTimeout(0);
+    expect(fixture.componentInstance.monacoEditorComponent.editor.getValue())
+      .toEqual(expectedCode);
+  });
+
+  it('Should be able to update code', async () => {
+    const newCode = 'Hello world!';
+    fixture.componentInstance.code = 'Hello world!';
+    fixture.detectChanges();
+    await asyncTimeout(0);
+    expect(fixture.componentInstance.monacoEditorComponent.editor.getValue())
+      .toEqual(newCode);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MonacoEditorComponent, MonacoEditorConstructionOptions, MonacoEditorLoaderService } from '@materia-ui/ngx-monaco-editor';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'monaco-testing';
+  @ViewChild(MonacoEditorComponent, { static: false })
+  monacoEditorComponent: MonacoEditorComponent;
+  editorOptions: MonacoEditorConstructionOptions = {
+    theme: "vs",
+    language: "json",
+    minimap: {
+      enabled: false
+    },
+    lineNumbers: "off",
+    automaticLayout: true
+  };
+  modelUri: monaco.Uri;
+  code = `{
+    "test": 1,
+    "scopes": { "test": 2 }
+}`;
+
+  constructor(private monacoLoaderService: MonacoEditorLoaderService) {
+    this.monacoLoaderService.isMonacoLoaded$
+      .pipe(
+        filter(isLoaded => isLoaded),
+        take(1)
+      )
+      .subscribe(() => {
+        console.log("loaded");
+        this.modelUri = monaco.Uri.parse("a://b/foo.json"); // a made up unique URI for our model
+        // this.model = monaco.editor.createModel(jsonCode, "json", modelUri);
+        monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+          enableSchemaRequest: true,
+          validate: true,
+          schemas: [
+            {
+              uri: "http://myserver/city-schema.json",
+              fileMatch: ["foo*.json"], // associate with our model
+              schema: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                  scopes: {
+                    description: "something useful here",
+                    type: "array",
+                    items: {
+                      type: "object",
+                      properties: {
+                        include: {
+                          type: "array",
+                          items: [
+                            {
+                              type: "string"
+                            }
+                          ]
+                        },
+                        exclude: {
+                          type: "array",
+                          items: [
+                            {
+                              type: "string"
+                            }
+                          ]
+                        },
+                        asset_types: {
+                          type: "array",
+                          items: [
+                            {
+                              type: "string"
+                            }
+                          ]
+                        }
+                      },
+                      required: ["include"]
+                    }
+                  }
+                },
+                required: ["scopes"]
+              }
+            }
+          ]
+        });
+      });
+  }
 }
